b69a6e6ea9485fb76915200cf9db428e864cbc0f
christopherbWIX <116498462+christopherbWIX@users.noreply.github.com>
2024-02-10 19:42:32 +0200

    Delete test-packages/call-comment directory

--- test-packages/call-comment/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-const createCommentBlock = require('code-commenter');
-
-const inputString = "This is a sample\n string.";
-
-const modifiedComment = createCommentBlock(inputString);
-
-console.log(modifiedComment);
